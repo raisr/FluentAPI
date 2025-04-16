854152e3c8e4c6a4de9c9f4a0ac99d4151c7b4e9
raisr <kuss.rene+gitlab@gmail.com>
2025-04-16 23:05:47 +0200

    Added yarp with initial configuration for demo api (#2)

--- src/FluentAPI.Web.UI/Program.cs
+++ src/FluentAPI.Web.UI/Program.cs
@@ -9,11 +9,16 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
         builder.AddServiceDefaults();
 
+        builder.Services.AddReverseProxy()
+            .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
         // Add services to the container.
         builder.Services.AddRazorComponents()
             .AddInteractiveServerComponents();
 
         var app = builder.Build();
+
+        app.MapReverseProxy();
 
         app.MapDefaultEndpoints();
 
