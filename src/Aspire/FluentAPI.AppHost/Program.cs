53034994fad3da12a43889c1e687636bdf383fea
raisr <kuss.rene+gitlab@gmail.com>
2025-04-16 22:24:53 +0200

    Added Demo API for testing stuff

--- src/Aspire/FluentAPI.AppHost/Program.cs
+++ src/Aspire/FluentAPI.AppHost/Program.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.FluentAPI_Web_UI>("fluentapi-web-ui");
 
+builder.AddProject<Projects.FluentApi_DemoApi>("fluentapi-demoapi");
+
 builder.Build().Run();
---
+++ src/Demo/FluentApi.DemoApi/Controllers/EchoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FluentApi.DemoApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EchoController : ControllerBase
+    {
+        [HttpGet("{message}")]
+        public IActionResult Get(string message) => Ok(new { Echo = message });
+
+        [HttpPost]
+        public IActionResult Post([FromBody] object data) => Ok(new { Received = data });
+    }
+
+}
